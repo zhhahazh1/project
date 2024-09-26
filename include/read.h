45b70b58c9b7d28bf1e8b0e1d61e73597de6cda7
zjj <761650489@qq.com>
2024-09-26 19:53:46 +0800

    master

--- include/read.h
+++ include/read.h
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <sstream>
 #include <regex> 
+#include <queue>
 // 读取节点文件
 NodeVector readNodes(const std::string filename) {
     std::ifstream infile(filename);
@@ -65,6 +66,7 @@ FpgaSet readFpgas(const std::string filename){
     std::string line;
     FpgaSet fpgas;
     std::regex fpga_regex("FPGA(\\d+)");
+    size_t maxId = 0;
     while (std::getline(infile, line)) {
         std::istringstream iss(line);
         std::string fpga_name;
@@ -74,34 +76,75 @@ FpgaSet readFpgas(const std::string filename){
         size_t id;
         std::regex_search(fpga_name, match, fpga_regex);
         id = std::stoi(match[1].str())-1; // 转换为整数
+        maxId = std::max(maxId, id + 1);
         for (int i = 0; i < 8; ++i) {
             iss >> area[i];
         }
         Fpga* fpga = new Fpga(area,id); 
         fpgas.push_back(fpga);
     }
-    return fpgas;
-}
-
-FpgaVector readtopo(const std::string filename, int fpga_max_dis){
-    std::ifstream infile(filename);
-    std::string line;
-    std::getline(infile, line);
-    fpga_max_dis=std::stoi(line);
-    std::regex fpga_regex("FPGA(\\d+)");
-    FpgaVector fpgas;
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        std::string fpga_name;
-        std::smatch match;
-        int id[2];
-        for (int i = 0; i < 2; ++i) {
-        iss >> fpga_name;
-        std::regex_search(fpga_name, match, fpga_regex);
-        id[0] = std::stoi(match[1].str())-1; 
-        }
-        fpgas[id[0]]->distance_neifpga[id[1]] = 1;
-        fpgas[id[1]]->distance_neifpga[id[0]] = 1;
+    for (auto fpga : fpgas) {
+        fpga->initial_Distance(maxId);
     }
     return fpgas;
 }
+
+FpgaVector readtopo(const std::string filename, int hop_max){
+    std::ifstream infile(filename);
+    std::string line;
+    std::getline(infile, line);
+    hop_max=std::stoi(line);
+    std::regex fpga_regex("FPGA(\\d+)");
+    FpgaVector fpgas;
+    std::vector<int>* adjList;
+    adjList = new std::vector<int>[fpgas[0]->numFpgas];//创建邻接表
+    while (std::getline(infile, line)) {
+        std::istringstream iss(line);
+        std::string fpga_name_1;
+        std::string fpga_name_2;
+        std::smatch match_1;
+        std::smatch match_2;
+        int id[2];
+        iss >> fpga_name_1;
+        std::regex_search(fpga_name_1, match_1, fpga_regex);
+        id[0] = std::stoi(match_1[1].str())-1; 
+        iss >> fpga_name_2;
+        std::regex_search(fpga_name_2, match_2, fpga_regex);
+        id[1] = std::stoi(match_2[1].str())-1; 
+        //fpgas[id[0]]->distance_neifpga[id[1]] = 1;
+        //fpgas[id[1]]->distance_neifpga[id[0]] = 1;
+        adjList[id[0]].push_back(id[1]);
+        adjList[id[1]].push_back(id[0]);
+    };
+    for (int i = 0; i < fpgas[0]->numFpgas; ++i){
+        BFS(i,adjList,fpgas);
+    }
+    return fpgas;
+}
+void BFS(int startVertex,std::vector<int>* adjList,FpgaVector fpgas) {
+        std::vector<bool> visited(fpgas[0]->numFpgas, false);   // 标记节点是否被访问
+        std::queue<int> queue;                           // 用于BFS的队列
+ 
+        // 将起始节点入队并标记为已访问
+        queue.push(startVertex);
+        visited[startVertex] = true;
+ 
+        while (!queue.empty()) {
+            int currentVertex = queue.front();
+            bool onlyOneElement = queue.size() == 1;//避免queue.size()变化影响判断
+            queue.pop();
+            if (onlyOneElement){
+                for (int i = 0; i < fpgas[0]->numFpgas; ++i) {
+                    if (!visited[i]) {
+                        fpgas[startVertex]->distance_neifpga[i] ++;
+                }
+            }}
+            // 遍历当前节点的相邻节点
+            for (int neighbor : adjList[currentVertex]) {
+                if (!visited[neighbor]) {
+                    queue.push(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+    }
