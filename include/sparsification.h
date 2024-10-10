06aac1e128d8c6bd8d59039abf7aa0acbd3c331c
zjj <761650489@qq.com>
2024-10-10 18:23:38 +0800

    master

--- include/sparsification.h
+++ include/sparsification.h
@@ -1,3 +1,94 @@
+#include <cstdint>
+#include <iostream>
+#define KAHYPAR_ATTRIBUTE_FALLTHROUGH __attribute__ ((fallthrough))
+template <typename Key>
+class MurmurHash {
+ public:
+  using HashValue = uint64_t;
+
+  explicit MurmurHash(const uint32_t seed = 0) :
+    _seed(seed)
+  { }
+
+  void reset(const uint32_t seed) {
+    _seed = seed;
+  }
+
+  inline HashValue operator() (const Key& key) const {
+    return hash(reinterpret_cast<const void*>(&key), sizeof(key), _seed);
+  }
+
+ private:
+  inline uint64_t hash(const void* key, const uint32_t len, const uint32_t seed) const {
+    const uint64_t m = 0xc6a4a7935bd1e995;
+    const int r = 47;
+
+    uint64_t h = seed ^ (len * m);
+
+    const uint64_t* data = (const uint64_t*)key;
+    const uint64_t* end = data + (len / 8);
+
+    while (data != end) {
+      uint64_t k = *data++;
+
+      k *= m;
+      k ^= k >> r;
+      k *= m;
+
+      h ^= k;
+      h *= m;
+    }
+
+    const unsigned char* data2 = (const unsigned char*)data;
+
+    switch (len & 7) {
+      case 7:
+        h ^= uint64_t(data2[6]) << 48;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 6:
+        h ^= uint64_t(data2[5]) << 40;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 5:
+        h ^= uint64_t(data2[4]) << 32;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 4:
+        h ^= uint64_t(data2[3]) << 24;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 3:
+        h ^= uint64_t(data2[2]) << 16;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 2:
+        h ^= uint64_t(data2[1]) << 8;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 1: h ^= uint64_t(data2[0]);
+        h *= m;
+    }
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+  }
+
+  uint32_t _seed;
+};
+
+using HashFunc = MurmurHash<uint32_t>;
+using HashValue = typename HashFunc::HashValue;
+Hypergraph buildSparsifiedHypergraph(const Hypergraph& hypergraph) {//生成稀疏化后的图
+    n;
+//生成sparsifier，计算哈希值（复合哈希值），哈希值全部相同的点放入同一个buckets 
+//合并顶点，去除多余边
+//cluster数在某个范围后，停止聚类
+}
+
+
+
+void hash_calculate(const Hypergraph& hypergraph){
+    HashFunc hash;
+    HashFuncVector<HashFunc> _hash_func_vector;//创建数组存放每个id对应的hash值，seed用什么标识区分？
+}
 
 void minihash(){
     std::srand(static_cast<unsigned int>(std::time(0))); // 设置随机种子
@@ -32,3 +123,4 @@ void sparsification(HyperGraph &HyperGraph){
     NodeVector Node_vector = HyperGraph.Node_vector;
     Node* node=*Node_vector[0]+*Node_vector[1];
 }
+
--- test.cpp
+++ test.cpp
@@ -1,176 +1,96 @@
-#include <fstream>
-#include <sstream>
-#include <regex> 
+#include <cstdint>
 #include <iostream>
+#define KAHYPAR_ATTRIBUTE_FALLTHROUGH __attribute__ ((fallthrough))
 #include <vector>
-#include <set>
-#include <algorithm>
-#include <utility>
-#include <queue>
 
-/*测试读入
-int main() {
-    std::string design_info = "/home/zwl/project/exapmle/design.are";
-    std::ifstream infile(design_info);
-    std::string line;
-    std::regex fpga_regex("g(\\d+)");
-    int a;
+template <typename Key>
+class MurmurHash {
+ public:
+  using HashValue = uint64_t;
 
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        std::string fpga_name;
-        int area[8];
-        iss >> fpga_name;
-        std::smatch match;
-        size_t id;
-        std::regex_search(fpga_name, match, fpga_regex);
-        id = std::stoi(match[1].str())-1; // 转换为整数，match[1] 返回第一个括号内的匹配子串。
-        for (int i = 0; i < 8; ++i) {
-            iss >> area[i];
-        }
-    }
-    return 0;
-}*/
+  explicit MurmurHash(const uint32_t seed = 0) :
+    _seed(seed)
+  { }
 
-//测试读取topo
-class Fpga;
-class Fpga {
-public:
-    Fpga(const int *area,size_t id) {  
-        for (int i = 0; i < 8; ++i) {
-            this->area[i] = area[i];
-        }
-        this->ID=id;
-        //this->distance_neifpga = nullptr;
+  void reset(const uint32_t seed) {
+    _seed = seed;
+  }
+
+  inline HashValue operator() (const Key& key) const {
+    return hash(reinterpret_cast<const void*>(&key), sizeof(key), _seed);
+  }
+
+ private:
+  inline uint64_t hash(const void* key, const uint32_t len, const uint32_t seed) const {
+    const uint64_t m = 0xc6a4a7935bd1e995;
+    const int r = 47;
+
+    uint64_t h = seed ^ (len * m);
+
+    const uint64_t* data = (const uint64_t*)key;
+    const uint64_t* end = data + (len / 8);
+
+    while (data != end) {
+      uint64_t k = *data++;
+
+      k *= m;
+      k ^= k >> r;
+      k *= m;
+
+      h ^= k;
+      h *= m;
     }
-    void initial_Distance(size_t numFpga) {
-        this->numFpgas=numFpga;
-        this->distance_neifpga.resize(numFpgas, 0);
-        adjList = new std::vector<int>[numFpgas];
+
+    const unsigned char* data2 = (const unsigned char*)data;
+
+    switch (len & 7) {
+      case 7:
+        h ^= uint64_t(data2[6]) << 48;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 6:
+        h ^= uint64_t(data2[5]) << 40;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 5:
+        h ^= uint64_t(data2[4]) << 32;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 4:
+        h ^= uint64_t(data2[3]) << 24;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 3:
+        h ^= uint64_t(data2[2]) << 16;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 2:
+        h ^= uint64_t(data2[1]) << 8;
+        KAHYPAR_ATTRIBUTE_FALLTHROUGH;
+      case 1: h ^= uint64_t(data2[0]);
+        h *= m;
     }
-    int area[8]; 
-    size_t ID;
-    std::vector<int> distance_neifpga;
-    std::vector<int>* adjList;
-    size_t numFpgas;
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+  }
+
+  uint32_t _seed;
 };
-
-using FpgaVector = std::vector<Fpga*>;
-FpgaVector readFpgas(const std::string filename){
-    std::ifstream infile(filename);
-    std::string line;
-    FpgaVector fpgas;
-    std::regex fpga_regex("FPGA(\\d+)");
-    size_t maxId = 0;
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        std::string fpga_name;
-        int area[8];
-        iss >> fpga_name;
-        std::smatch match;
-        size_t id;
-        std::regex_search(fpga_name, match, fpga_regex);
-        id = std::stoi(match[1].str())-1; // 转换为整数
-        maxId = std::max(maxId, id + 1);
-        for (int i = 0; i < 8; ++i) {
-            iss >> area[i];
-        }
-        Fpga* fpga = new Fpga(area,id); 
-        fpgas.push_back(fpga);
+  HashValue combinedHash(const Hypergraph& graph, const VertexId vertex_id) const {
+    ALWAYS_ASSERT(getHashNum() > 0, "The number of hashes should be greater than zero");
+    HashValue val = 0;
+    for (uint32_t hash_num = 0; hash_num < _hash_func_vector.getHashNum(); ++hash_num) {
+      val ^= minHash(_hash_func_vector[hash_num], graph.incidentEdges(vertex_id));
     }
-    for (auto fpga : fpgas) {
-        fpga->initial_Distance(maxId);
-    }
-    return fpgas;
-}
+    return val;
+  }
+int main(){
+    using HashFunc = MurmurHash<uint32_t>;
+    using HashValue = typename HashFunc::HashValue;
+    HashFunc hash(8);
+    HashFunc hash1;
+    HashFunc hash2;
+    HashValue a=hash(3);
+    HashValue b=hash(4);
+    std::vector<HashFunc> _hash_functions;
 
-void readtopo(const std::string filename, int fpga_max_dis){
-    std::ifstream infile(filename);
-    std::string line;
-    std::getline(infile, line);
-    fpga_max_dis=std::stoi(line);
-    std::regex fpga_regex("FPGA(\\d+)");
-    FpgaVector fpgas;
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        std::string fpga_name;
-        std::smatch match;
-        int id[2];
-        for (int i = 0; i < 2; ++i) {
-        iss >> fpga_name;
-        std::regex_search(fpga_name, match, fpga_regex);
-        id[0] = std::stoi(match[1].str())-1; 
-        }
-        fpgas[id[0]]->distance_neifpga[id[1]] = 1;
-        fpgas[id[1]]->distance_neifpga[id[0]] = 1;
-    }
-}
-void BFS(int startVertex,std::vector<int>* adjList,FpgaVector fpgas) {
-        std::vector<bool> visited(fpgas[0]->numFpgas, false);   // 标记节点是否被访问
-        std::queue<int> queue;                           // 用于BFS的队列
-        std::queue<int> tem_queue;
- 
-        // 将起始节点入队并标记为已访问
-        queue.push(startVertex);
-        visited[startVertex] = true;
- 
-        while (!queue.empty()) {
-            int currentVertex = queue.front();
-            //bool onlyOneElement = queue.size() == 1;//避免queue.size()变化影响判断
-            queue.pop();
-            for (int neighbor : adjList[currentVertex]) {
-                if (!visited[neighbor]) {
-                    tem_queue.push(neighbor);
-                }
-            }
-            if (queue.empty()) {
-                for (int i = 0; i < fpgas[0]->numFpgas; ++i) {
-                    if (!visited[i]) {
-                        fpgas[startVertex]->distance_neifpga[i] ++;
-                }
-            }
-               while (!tem_queue.empty()) {
-                int nextVertex = tem_queue.front();
-                tem_queue.pop();
-                queue.push(nextVertex);
-                visited[nextVertex] = true;
-        }
-    }}}
-
-int main() {
-    std::string design_info = "/home/zwl/project/exapmle/design.info";
-    FpgaVector fpgas = readFpgas(design_info);
-    std::string design_topo = "/home/zwl/project/exapmle/design.topo";
-    int hop_max;
-    std::ifstream infile(design_topo);
-    std::string line;
-    std::getline(infile, line);
-    hop_max=std::stoi(line);
-    std::regex fpga_regex("FPGA(\\d+)");
-
-    std::vector<int>* adjList;
-    adjList = new std::vector<int>[fpgas[0]->numFpgas];//创建邻接表
-
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        std::string fpga_name_1;
-        std::string fpga_name_2;
-        std::smatch match_1;
-        std::smatch match_2;
-        int id[2];
-        iss >> fpga_name_1;
-        std::regex_search(fpga_name_1, match_1, fpga_regex);
-        id[0] = std::stoi(match_1[1].str())-1; 
-        iss >> fpga_name_2;
-        std::regex_search(fpga_name_2, match_2, fpga_regex);
-        id[1] = std::stoi(match_2[1].str())-1; 
-        //fpgas[id[0]]->distance_neifpga[id[1]] = 1;
-        //fpgas[id[1]]->distance_neifpga[id[0]] = 1;
-        adjList[id[0]].push_back(id[1]);
-        adjList[id[1]].push_back(id[0]);
-    };
-    for (int i = 0; i < fpgas[0]->numFpgas; ++i){
-        BFS(i,adjList,fpgas);
-    }
-    return 0;
 }
