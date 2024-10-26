5e5abefa384c51e7f42335e118541f1ababa0632
zjj <761650489@qq.com>
2024-10-26 15:25:07 +0800

    master

--- test/test.cpp
+++ test/test.cpp
@@ -11,8 +11,12 @@ int main(){
     std::string design_topo = "../exapmle/case01/design.topo";
     NodeVector nodes = readNodes(design_aera);
     HyperedgeSet Hyperedge =readEdges(design_net, nodes);
-    HyperGraph HyperGraph(nodes,Hyperedge);                                                                                                                                                                                     
-    buildSparsifiedHypergraph(HyperGraph,3);
+    HyperGraph HyperGraph(nodes,Hyperedge);                                                                                                                                                                                  
+    buildSparsifiedHypergraph(HyperGraph,10);
+    NodeVector _nodes=HyperGraph.Node_vector;
+    size_t n=_nodes.size();
     desparseHypergraph(HyperGraph);
+    desparseNodes(_nodes);
+    size_t n2=_nodes.size();
     return 0;
     }
